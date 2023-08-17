d97e398f98bd9e5eedb1033b000b8677ec9a4c0b
MohamedShiiraar <mso2shiiraar@gmail.com>
2023-08-17 19:22:11 +0200

    Template 1

--- app/src/main/java/com/example/quickgigs/ActiveJobsActivity.java
+++ app/src/main/java/com/example/quickgigs/ActiveJobsActivity.java
@@ -1,4 +1,13 @@
 package com.example.quickgigs;
 
-public class ActiveJobsActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActiveJobsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activejobs);
+    }
 }
