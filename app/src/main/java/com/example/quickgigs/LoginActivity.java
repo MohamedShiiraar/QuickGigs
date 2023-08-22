b56a97e4eea75d78d3684fe3a72d7cf4a87bf8fc
Icewm147 <127603940+Icewm147@users.noreply.github.com>
2023-08-22 08:12:03 -0400

    functionality prototype
    Login and Register code added to existing code

--- app/src/main/java/com/example/quickgigs/LoginActivity.java
+++ app/src/main/java/com/example/quickgigs/LoginActivity.java
@@ -4,11 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import dao.UserAndJobDAOImpl;
+import model.User;
+
 public class LoginActivity extends AppCompatActivity {
     private Button btnLogin2;
+    private EditText edtEmail, edtPassword1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +22,25 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnLogin2= findViewById(R.id.btnLogin2);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword1 = findViewById(R.id.edtPassword1);
         btnLogin2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openHome();
 
+                String email = edtEmail.getText().toString();
+                String password = edtPassword1.getText().toString();
+
+                User user = new User();
+                user.setEmailaddress(email);
+                user.setPassword(password);
+
+                UserAndJobDAOImpl userDao = new UserAndJobDAOImpl(LoginActivity.this);
+                if (userDao.login(user)) {
+                    openHome();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login failed. Invalid credentials.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
