26515b2a1f94d37a30ca396605b41ed61a137c88
MohamedShiiraar <mso2shiiraar@gmail.com>
2023-08-24 22:00:26 +0200

    Template Complete

--- app/src/main/java/com/example/quickgigs/ActivityHome.java
+++ app/src/main/java/com/example/quickgigs/ActivityHome.java
@@ -2,18 +2,47 @@ package com.example.quickgigs;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ActivityHome extends AppCompatActivity {
     private Button btnCreateJobList;
+    private BottomNavigationView bottomNavView;
+    private String authenticatedUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btnCreateJobList = findViewById(R.id.btnCreateJob);
+        bottomNavView = findViewById(R.id.bottomNavigationView);
+        bottomNavView.setSelectedItemId(R.id.nav_home);
+
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.nav_home:
+                        return true;
+                    case R.id.nav_profile:
+                        Intent goToProjectActivityIntent = new Intent(ActivityHome.this, ProfileActivity.class);
+                        moveToIntent(goToProjectActivityIntent);
+                        return true;
+                    case R.id.nav_explore:
+                        Intent goToExploreActivityIntent = new Intent(ActivityHome.this, ExploreActivity.class);
+                        moveToIntent(goToExploreActivityIntent);
+                        return true;
+                }
+                return false;
+            }
+        });
 
         btnCreateJobList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +54,10 @@ public class ActivityHome extends AppCompatActivity {
     }
     public void openAddJob() {
         Intent intent = new Intent(this,AddJobsActivity.class);
+        startActivity(intent);
+    }
+    private void moveToIntent(Intent intent) {
+        intent.putExtra("authenticatedUser", authenticatedUser);
         startActivity(intent);
     }
 }
