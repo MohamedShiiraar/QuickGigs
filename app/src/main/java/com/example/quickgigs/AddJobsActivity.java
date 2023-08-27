8b0e5c3e52643213b7fb3944b85d2de08757d5d6
Icewm147 <127603940+Icewm147@users.noreply.github.com>
2023-08-27 03:52:26 -0400

    commit AddJobActivity

--- app/src/main/java/com/example/quickgigs/AddJobsActivity.java
+++ app/src/main/java/com/example/quickgigs/AddJobsActivity.java
@@ -3,12 +3,42 @@ package com.example.quickgigs;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.*;
+import androidx.appcompat.app.AppCompatActivity;
+import dao.UserAndJobDAOImpl;
+import model.User;
+import com.example.quickgigs.Utilities.DBStrings;
+import com.example.quickgigs.dao.UserAndJobsDAOImpl;
 
 public class AddJobsActivity extends AppCompatActivity {
+
+    private EditText edtJobTitle;
+    private EditText edtRatePerHour;
+    private EditText edtContactNumber;
+    private EditText edtArea;
+    private Button btnPost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addjob);
 
+        edtJobTitle = findViewById(R.id.edtJobTitle);
+        edtRatePerHour = findViewById(R.id.edtRatePerHour);
+        edtContactNumber = findViewById(R.id.edtContactNumber);
+        edtArea = findViewById(R.id.edtArea);
+        btnPost = findViewById(R.id.btnPost);
+
+        userAndJobDAOImpl = new UserAndJobsDAOImpl(this);
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addJob();
+            }
+        });
     }
 }
